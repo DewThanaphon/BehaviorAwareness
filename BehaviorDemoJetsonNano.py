ac27daec23b8756c145898b2e7ec164e4b8bf61e
DewThanaphon <92207106+DewThanaphon@users.noreply.github.com>
2022-06-29 15:31:14 +0700

    Update BehaviorDemoJetsonNano.py

--- BehaviorDemoJetsonNano.py
+++ BehaviorDemoJetsonNano.py
@@ -233,8 +233,6 @@ def main():
     datum = op.Datum()
 
     rf = joblib.load("model.joblib")
-    postures = ['working', 'thinking', 'lyingDown', 'relaxing', 'raiseHands']
-    
     postures = ['RaiseHands', 'LieDown', 'Working', 'Thinking', 'Relaxing']
 
     try:
